831f8285f9c04b59aa7105e70589eaacacc273ee
viras <viras@users.sourceforge.net>
2012-06-21 09:55:06 +0200

    custom thread class for making use of threadlistener interface

---
+++ jacq-servlet/src/java/at/ac/nhm_wien/jacq/ImageServerThread.java
@@ -0,0 +1,47 @@
+/**
+ * Basic thread implementation which supports listeners
+ */
+package at.ac.nhm_wien.jacq;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author wkoller
+ */
+public abstract class ImageServerThread extends Thread {
+    private ArrayList<ThreadListener> m_listeners = new ArrayList<ThreadListener>();
+    private int thread_id = 0;
+
+    public int getThread_id() {
+        return thread_id;
+    }
+
+    public void setThread_id(int thread_id) {
+        this.thread_id = thread_id;
+    }
+    
+    public void registerListener(ThreadListener listener) {
+        this.m_listeners.add(listener);
+    }
+    
+    public void removeListener(ThreadListener listener) {
+        this.m_listeners.remove(listener);
+    }
+    
+    protected void notifyListeners() {
+        for( ThreadListener listener : m_listeners ) {
+            listener.threadCompleted(this);
+        }
+    }
+    
+    protected void messageListeners(String message) {
+        this.messageListeners("", message);
+    }
+    
+    protected void messageListeners(String identifier, String message) {
+        for( ThreadListener listener : m_listeners ) {
+            listener.threadMessage(this, identifier, message);
+        }
+    }
+}
